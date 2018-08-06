b7f03130e6017acbed86ff191595cd1691ac2332
Maor Rosenfeld <maor.r@walkme.com>
2018-08-07 01:34:10 +0300

    Add a map

--- app/lost-pet/lost-pet.component.ts
+++ app/lost-pet/lost-pet.component.ts
@@ -6,6 +6,7 @@ import { Guid } from "guid-typescript";
 import { LostPetsReporterService } from '~/services/lost-pets-reporter.service';
 import * as app from "application";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { MapView } from 'nativescript-google-maps-sdk';
 
 @Component({
   selector: 'lost-pet',
@@ -16,6 +17,18 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 export class LostPetComponent implements OnInit {
   private _lostPet: LostPet;
 
+  latitude =  32.0672359;
+  longitude = 34.7947387;
+  zoom = 18;
+  minZoom = 0;
+  maxZoom = 22;
+  bearing = 0;
+  tilt = 0;
+  padding = [40, 40, 40, 40];
+  mapView: MapView & { infoWindowTemplates: string };
+  lastCamera: String;
+
+  
   constructor(private lostPetsReporterService: LostPetsReporterService) {
     
   }
@@ -26,6 +39,10 @@ export class LostPetComponent implements OnInit {
 
   get lostPet(): LostPet {
     return this._lostPet;
+  }
+  onMapReady(event) {
+    console.log('Map Ready, retrieving pets...');
+    this.mapView = event.object; 
   }
 
   onTap(value: string): void {
