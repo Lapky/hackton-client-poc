27b1ff946e65cad712547caecbdf9e852ac18d83
rita.g <rita.g@walkme.com>
2018-08-06 15:54:02 +0300

    deleted unneeded stuff

--- app/settings/settings.module.ts
+++ app/settings/settings.module.ts
@@ -1,15 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
-import { DropDownModule } from "nativescript-drop-down/angular";
-
 import { SettingsRoutingModule } from "./settings-routing.module";
 import { SettingsComponent } from "./settings.component";
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
-        SettingsRoutingModule,
-        DropDownModule
+        SettingsRoutingModule
     ],
     declarations: [
         SettingsComponent
