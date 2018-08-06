af6a4fa3b48cc66d1a3b6e3d9a1c76b8e8cede86
rita.g <rita.g@walkme.com>
2018-08-06 18:31:51 +0300

    Added image picker

--- app/sos-report/sos-report.component.ts
+++ app/sos-report/sos-report.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import * as app from "application";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { Mediafilepicker, MediaFilepickerOptions } from 'nativescript-mediafilepicker';
+const imageSourceModule = require("tns-core-modules/image-source");
+const fileSystemModule = require("tns-core-modules/file-system");
 
 @Component({
   selector: 'sos-report',
@@ -9,11 +12,50 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
   providers: []
 })
 export class SosReportComponent {
+  public pictureFromCamera: string;
+  private mediafilepicker : Mediafilepicker; 
+
   constructor() {
+    this.mediafilepicker = new Mediafilepicker();
   }
 
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>app.getRootView();
     sideDrawer.showDrawer();
   }  
+  
+  takePicture() {
+    let options: MediaFilepickerOptions = {
+      android: {
+          mxcount: 2,
+          enableImagePicker: true,
+          enableVideoPicker: false,
+          enableCameraSupport: true,
+      },
+      ios: {
+          allowsMultipleSelection: false,
+          title: "Album",
+          showCameraButton: true,
+      }
+  };
+  this.mediafilepicker.on("getFiles", function (res: any) {
+      let files = res.files;
+
+      if (files.length > 0) {
+          files = files.split(",");
+
+          files.forEach(file => {
+              const imageFromLocalFile = imageSourceModule.fromFile(file);
+
+              console.log("imageFromLocalFile", imageFromLocalFile)
+         
+              this.pictureFromCamera = JSON.stringify(imageFromLocalFile);
+          });
+      } else {
+          console.log("There was some problem to select the file. Looks like user has cancel it.")
+      }
+
+  })
+  this.mediafilepicker.startFilePicker(options);
+  }
 }
